bc6d5a11f1db68f7d97024e8e7f16dd162857cf8
nicoarqu <nicolas.araya@uam.es>
2024-11-04 17:22:40 +0100

    🚧 update test_black.py

--- tests/test_black.py
+++ tests/test_black.py
@@ -19,17 +19,15 @@ def simple_sum(x,       y):
     return x + y
 
 
-def get_trigonometry(angle):
-    sin = math.sin(angle)
-    cos = math.cos(angle)
-    tan = math.tan(angle)
-    return (sin, cos, tan)
-
-
-def long_static_function(
-    parameter1, parameter2, parameter3, parameter4, parameter5, parameter6
-):
-    result_string = (
-        "Esta frase vemos que supera con creces el límite de los 80 caracteres!!"
-    )
+def long_static_function(parameter1, parameter2, parameter3, parameter4, parameter5, parameter6):
+    result_string = "Esta frase vemos que supera con creces el límite de los 80 caracteres!!"
     return result_string
+
+def get_trigonometry(angle):
+    sin = math.sin(angle)#first comment inline
+    cos = math.cos(angle)
+    tan = math.tan(angle)#comment
+    return (
+        sin, 
+        cos,
+        tan)
